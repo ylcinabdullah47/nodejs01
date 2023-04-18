30843d818668e6ddf46023c98b8278e0d6c54bd4
abdullah <a.ylcin47@gmail.com>
2023-04-18 19:02:53 +0300

    mysql ve mongodb verii tabanı bağlantısı yapıldı  diğer işelmer yapılacak

---
+++ mongoDb/app.js
@@ -0,0 +1,11 @@
+const mongodb = require('mongodb');
+const client =mongodb.MongoClient;
+const url='mongodb://127.0.0.1:27017'
+client.connect(url,(err,db)=>{
+    console.log('err',err);
+    console.log('db',db);
+    const mydb=db.db('egitimdb')
+    mydb.createCollection('deneme').then((r)=>{
+        console.log('r',r);
+    })
+})
